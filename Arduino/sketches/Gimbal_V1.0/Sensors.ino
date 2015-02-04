f3eabd3f6138de0ab81cfa87c9d9b27b602f606a
arlarson-tuska <arlarson@crimson.ua.edu>
2015-02-03 19:25:02 -0600

    add gimbal v1.0

---
+++ Arduino/sketches/Gimbal_V1.0/Sensors.ino
@@ -0,0 +1,368 @@
+// Sensor.ino
+
+#define GYRO 1
+#define ACC 1
+
+#define ROLL       0
+#define PITCH      1
+#define YAW        2
+
+static struct {
+  uint8_t checkNewConf;
+  int16_t accZero[3];
+  int16_t angleTrim[2];
+  #if defined(GYRO_SMOOTHING)
+    uint8_t Smoothing[3];
+  #endif
+} conf;
+
+#define MPU6050_ADDRESS     0x68 // address pin AD0 low (GND), default for FreeIMU v0.4 and InvenSense evaluation board
+
+#if !defined(ACC_ORIENTATION) 
+  #define ACC_ORIENTATION(X, Y, Z)  {accADC[ROLL]  = X; accADC[PITCH]  = Y; accADC[YAW]  = Z;}
+#endif
+#if !defined(GYRO_ORIENTATION) 
+  #define GYRO_ORIENTATION(X, Y, Z) {gyroADC[ROLL] = X; gyroADC[PITCH] = Y; gyroADC[YAW] = Z;}
+#endif
+
+//MPU6050 Gyro LPF setting
+#if defined(MPU6050_LPF_256HZ) || defined(MPU6050_LPF_188HZ) || defined(MPU6050_LPF_98HZ) || defined(MPU6050_LPF_42HZ) || defined(MPU6050_LPF_20HZ) || defined(MPU6050_LPF_10HZ) || defined(MPU6050_LPF_5HZ)
+  #if defined(MPU6050_LPF_256HZ)
+    #define MPU6050_DLPF_CFG   0
+  #endif
+  #if defined(MPU6050_LPF_188HZ)
+    #define MPU6050_DLPF_CFG   1
+  #endif
+  #if defined(MPU6050_LPF_98HZ)
+    #define MPU6050_DLPF_CFG   2 
+  #endif
+  #if defined(MPU6050_LPF_42HZ)
+    #define MPU6050_DLPF_CFG   3
+  #endif
+  #if defined(MPU6050_LPF_20HZ)
+    #define MPU6050_DLPF_CFG   4
+  #endif
+  #if defined(MPU6050_LPF_10HZ)
+    #define MPU6050_DLPF_CFG   5
+  #endif
+  #if defined(MPU6050_LPF_5HZ)
+    #define MPU6050_DLPF_CFG   6
+  #endif
+#else
+   //Default settings LPF 256Hz/8000Hz sample
+    #define MPU6050_DLPF_CFG   0
+#endif
+
+#define I2C_SPEED 100000L     //100kHz normal mode, this value must be used for a genuine WMP
+static uint32_t neutralizeTime = 0;
+static int16_t  i2c_errors_count = 0;
+uint8_t rawADC[6];
+
+// ************************************************************************************************************
+// I2C Helper Functions
+// ************************************************************************************************************
+
+void i2c_init(void) {
+   // I2C_PULLUPS_DISABLE
+  TWSR = 0;                                    // no prescaler => prescaler = 1
+  TWBR = ((F_CPU / I2C_SPEED) - 16) / 2;   // change the I2C clock rate
+  TWCR = 1<<TWEN;                              // enable twi module, no interrupt
+}
+
+void i2c_rep_start(uint8_t address) {
+  TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN) ; // send REPEAT START condition
+  waitTransmissionI2C();                       // wait until transmission completed
+  TWDR = address;                              // send device address
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  waitTransmissionI2C();                       // wail until transmission completed
+}
+
+void i2c_stop(void) {
+  TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+  //  while(TWCR & (1<<TWSTO));                // <- can produce a blocking state with some WMP clones
+}
+
+void i2c_write(uint8_t data ) { 
+  TWDR = data;                                 // send data to the previously addressed device
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  waitTransmissionI2C();
+}
+
+uint8_t i2c_read(uint8_t ack) {
+  TWCR = (1<<TWINT) | (1<<TWEN) | (ack? (1<<TWEA) : 0);
+  waitTransmissionI2C();
+  uint8_t r = TWDR;
+  if (!ack) i2c_stop();
+  return r;
+}
+
+uint8_t i2c_readAck() {
+  return i2c_read(1);
+}
+
+uint8_t i2c_readNak(void) {
+  return i2c_read(0);
+}
+
+void waitTransmissionI2C() {
+  uint16_t count = 255;
+  while (!(TWCR & (1<<TWINT))) {
+    count--;
+    if (count==0) {              //we are in a blocking state => we don't insist
+      TWCR = 0;                  //and we force a reset on TWINT register
+      neutralizeTime = micros(); //we take a timestamp here to neutralize the value during a short delay
+      i2c_errors_count++;
+      break;
+    }
+  }
+}
+
+size_t i2c_read_to_buf(uint8_t add, void *buf, size_t size) {
+  i2c_rep_start((add<<1) | 1);  // I2C read direction
+  size_t bytes_read = 0;
+  uint8_t *b = (uint8_t*)buf;
+  while (size--) {
+    /* acknowledge all but the final byte */
+    *b++ = i2c_read(size > 0);
+    /* TODO catch I2C errors here and abort */
+    bytes_read++;
+  }
+  return bytes_read;
+}
+
+size_t i2c_read_reg_to_buf(uint8_t add, uint8_t reg, void *buf, size_t size) {
+  i2c_rep_start(add<<1); // I2C write direction
+  i2c_write(reg);        // register selection
+  return i2c_read_to_buf(add, buf, size);
+}
+
+/* transform a series of bytes from big endian to little
+   endian and vice versa. */
+void swap_endianness(void *buf, size_t size) {
+  /* we swap in-place, so we only have to
+  * place _one_ element on a temporary tray
+  */
+  uint8_t tray;
+  uint8_t *from;
+  uint8_t *to;
+  /* keep swapping until the pointers have assed each other */
+  for (from = (uint8_t*)buf, to = &from[size-1]; from < to; from++, to--) {
+    tray = *from;
+    *from = *to;
+    *to = tray;
+  }
+}
+
+void i2c_getSixRawADC(uint8_t add, uint8_t reg) {
+  i2c_read_reg_to_buf(add, reg, &rawADC, 6);
+}
+
+void i2c_writeReg(uint8_t add, uint8_t reg, uint8_t val) {
+  i2c_rep_start(add<<1); // I2C write direction
+  i2c_write(reg);        // register selection
+  i2c_write(val);        // value to write in register
+  i2c_stop();
+}
+
+uint8_t i2c_readReg(uint8_t add, uint8_t reg) {
+  uint8_t val;
+  i2c_read_reg_to_buf(add, reg, &val, 1);
+  return val;
+}
+// ************************************************************************************************************
+
+
+// ****************
+// GYRO common part
+// ****************
+void GYRO_Common() {
+  static int16_t previousGyroADC[3] = {0,0,0};
+  static int32_t g[3];
+  uint8_t axis;
+  
+#if defined MMGYRO       
+  // Moving Average Gyros by Magnetron1
+  //---------------------------------------------------
+  static int16_t mediaMobileGyroADC[3][MMGYROVECTORLENGHT];
+  static int32_t mediaMobileGyroADCSum[3];
+  static uint8_t mediaMobileGyroIDX;
+  //---------------------------------------------------
+#endif
+
+  if (calibratingG>0) {
+    for (axis = 0; axis < 3; axis++) {
+      // Reset g[axis] at start of calibration
+      if (calibratingG == 400) g[axis]=0;
+      // Sum up 400 readings
+      g[axis] +=gyroADC[axis];
+      // Clear global variables for next reading
+      gyroADC[axis]=0;
+      gyroZero[axis]=0;
+      if (calibratingG == 1) {
+        gyroZero[axis]=g[axis]/400;
+        //blinkLED(10,15,1); //don't really need led for our purposes
+      }
+    }
+    calibratingG--;
+  }
+
+#ifdef MMGYRO       
+  mediaMobileGyroIDX = ++mediaMobileGyroIDX % MMGYROVECTORLENGHT;
+  for (axis = 0; axis < 3; axis++) {
+    gyroADC[axis]  -= gyroZero[axis];
+    mediaMobileGyroADCSum[axis] -= mediaMobileGyroADC[axis][mediaMobileGyroIDX];
+    //anti gyro glitch, limit the variation between two consecutive readings
+    mediaMobileGyroADC[axis][mediaMobileGyroIDX] = constrain(gyroADC[axis],previousGyroADC[axis]-800,previousGyroADC[axis]+800);
+    mediaMobileGyroADCSum[axis] += mediaMobileGyroADC[axis][mediaMobileGyroIDX];
+    gyroADC[axis] = mediaMobileGyroADCSum[axis] / MMGYROVECTORLENGHT;
+#else 
+  for (axis = 0; axis < 3; axis++) {
+    gyroADC[axis]  -= gyroZero[axis];
+    //anti gyro glitch, limit the variation between two consecutive readings
+    gyroADC[axis] = constrain(gyroADC[axis],previousGyroADC[axis]-800,previousGyroADC[axis]+800);
+#endif    
+    previousGyroADC[axis] = gyroADC[axis];
+  }
+}
+
+// ****************
+// ACC common part
+// ****************
+void ACC_Common() {
+  static int32_t a[3];
+  
+  if (calibratingA>0) {
+    for (uint8_t axis = 0; axis < 3; axis++) {
+      // Reset a[axis] at start of calibration
+      if (calibratingA == 400) a[axis]=0;
+      // Sum up 400 readings
+      a[axis] +=accADC[axis];
+      // Clear global variables for next reading
+      accADC[axis]=0;
+      conf.accZero[axis]=0;
+    }
+    // Calculate average, shift Z down by acc_1G and store values in EEPROM at end of calibration
+    if (calibratingA == 1) {
+      conf.accZero[ROLL]  = a[ROLL]/400;
+      conf.accZero[PITCH] = a[PITCH]/400;
+      conf.accZero[YAW]   = a[YAW]/400-acc_1G; // for nunchuk 200=1G
+      conf.angleTrim[ROLL]   = 0;
+      conf.angleTrim[PITCH]  = 0;
+      writeParams(1); // write accZero in EEPROM
+    }
+    calibratingA--;
+  }
+  #if defined(INFLIGHT_ACC_CALIBRATION)
+      static int32_t b[3];
+      static int16_t accZero_saved[3]  = {0,0,0};
+      static int16_t  angleTrim_saved[2] = {0, 0};
+      //Saving old zeropoints before measurement
+      if (InflightcalibratingA==50) {
+         accZero_saved[ROLL]  = conf.accZero[ROLL] ;
+         accZero_saved[PITCH] = conf.accZero[PITCH];
+         accZero_saved[YAW]   = conf.accZero[YAW] ;
+         angleTrim_saved[ROLL] = conf.angleTrim[ROLL] ;
+         angleTrim_saved[PITCH] = conf.angleTrim[PITCH] ;
+      }
+      if (InflightcalibratingA>0) {
+        for (uint8_t axis = 0; axis < 3; axis++) {
+          // Reset a[axis] at start of calibration
+          if (InflightcalibratingA == 50) b[axis]=0;
+          // Sum up 50 readings
+          b[axis] +=accADC[axis];
+          // Clear global variables for next reading
+          accADC[axis]=0;
+          conf.accZero[axis]=0;
+        }
+        //all values are measured
+        if (InflightcalibratingA == 1) {
+          AccInflightCalibrationActive = 0;
+          AccInflightCalibrationMeasurementDone = 1;
+          #if defined(BUZZER)
+            toggleBeep = 2;      //buzzer for indicatiing the end of calibration
+          #endif
+          // recover saved values to maintain current flight behavior until new values are transferred
+          conf.accZero[ROLL]  = accZero_saved[ROLL] ;
+          conf.accZero[PITCH] = accZero_saved[PITCH];
+          conf.accZero[YAW]   = accZero_saved[YAW] ;
+          conf.angleTrim[ROLL] = angleTrim_saved[ROLL] ;
+          conf.angleTrim[PITCH] = angleTrim_saved[PITCH] ;
+        }
+        InflightcalibratingA--;
+      }
+      // Calculate average, shift Z down by acc_1G and store values in EEPROM at end of calibration
+      if (AccInflightCalibrationSavetoEEProm == 1){  //the copter is landed, disarmed and the combo has been done again
+        AccInflightCalibrationSavetoEEProm = 0;
+        conf.accZero[ROLL]  = b[ROLL]/50;
+        conf.accZero[PITCH] = b[PITCH]/50;
+        conf.accZero[YAW]   = b[YAW]/50-acc_1G; // for nunchuk 200=1G
+        conf.angleTrim[ROLL]   = 0;
+        conf.angleTrim[PITCH]  = 0;
+        writeParams(1); // write accZero in EEPROM
+      }
+  #endif
+  accADC[ROLL]  -=  conf.accZero[ROLL] ;
+  accADC[PITCH] -=  conf.accZero[PITCH];
+  accADC[YAW]   -=  conf.accZero[YAW] ;
+}
+
+// ************************************************************************************************************
+// EEPROM Functions
+// ************************************************************************************************************
+#define EEPROM_CONF_VERSION 161
+
+void writeParams(uint8_t b) {
+  conf.checkNewConf = EEPROM_CONF_VERSION; // make sure we write the current version into eeprom
+  eeprom_write_block((const void*)&conf, (void*)0, sizeof(conf));
+  //readEEPROM();
+  //if (b == 1) blinkLED(15,20,1);
+}
+// ************************************************************************************************************
+
+// ************************************************************************************************************
+// I2C Gyroscope and Accelerometer MPU6050
+// ************************************************************************************************************
+
+void Gyro_init() {
+  TWBR = ((F_CPU / 400000L) - 16) / 2; // change the I2C clock rate to 400kHz
+  i2c_writeReg(MPU6050_ADDRESS, 0x6B, 0x80);             //PWR_MGMT_1    -- DEVICE_RESET 1
+  delay(5);
+  i2c_writeReg(MPU6050_ADDRESS, 0x6B, 0x03);             //PWR_MGMT_1    -- SLEEP 0; CYCLE 0; TEMP_DIS 0; CLKSEL 3 (PLL with Z Gyro reference)
+  i2c_writeReg(MPU6050_ADDRESS, 0x1A, MPU6050_DLPF_CFG); //CONFIG        -- EXT_SYNC_SET 0 (disable input pin for data sync) ; default DLPF_CFG = 0 => ACC bandwidth = 260Hz  GYRO bandwidth = 256Hz)
+  i2c_writeReg(MPU6050_ADDRESS, 0x1B, 0x18);             //GYRO_CONFIG   -- FS_SEL = 3: Full scale set to 2000 deg/sec
+  // enable I2C bypass for AUX I2C
+}
+
+void Gyro_getADC () {
+  i2c_getSixRawADC(MPU6050_ADDRESS, 0x43);
+  GYRO_ORIENTATION( ((rawADC[0]<<8) | rawADC[1])/4 , // range: +/- 8192; +/- 2000 deg/sec
+	            ((rawADC[2]<<8) | rawADC[3])/4 ,
+	            ((rawADC[4]<<8) | rawADC[5])/4 );
+  GYRO_Common();
+}
+
+void ACC_init () {
+  i2c_writeReg(MPU6050_ADDRESS, 0x1C, 0x10);             //ACCEL_CONFIG  -- AFS_SEL=2 (Full Scale = +/-8G)  ; ACCELL_HPF=0   //note something is wrong in the spec.
+  //note: something seems to be wrong in the spec here. With AFS=2 1G = 4096 but according to my measurement: 1G=2048 (and 2048/8 = 256)
+  //confirmed here: http://www.multiwii.com/forum/viewtopic.php?f=8&t=1080&start=10#p7480
+    acc_1G = 512;
+}
+
+void ACC_getADC () {
+  i2c_getSixRawADC(MPU6050_ADDRESS, 0x3B);
+  ACC_ORIENTATION( ((rawADC[0]<<8) | rawADC[1])/8 ,
+                   ((rawADC[2]<<8) | rawADC[3])/8 ,
+                   ((rawADC[4]<<8) | rawADC[5])/8 );
+  ACC_Common();
+}
+
+// ************************************************************************************************************
+void initSensors() {
+  delay(200);
+  delay(100);
+  i2c_init();
+  delay(100);
+  if (GYRO) Gyro_init();
+  if (ACC) {ACC_init();acc_25deg = acc_1G * 0.423;}
+  //f.I2C_INIT_DONE = 1;
+}
