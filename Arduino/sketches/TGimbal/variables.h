870de1442e4f3e943c869311598561cd48d1ce39
arlarson-tuska <arlarson@crimson.ua.edu>
2015-05-14 14:20:02 -0700

    change variables to static

--- Arduino/sketches/TGimbal/variables.h
+++ Arduino/sketches/TGimbal/variables.h
@@ -1,21 +1,26 @@
-#ifndef variable_h
-#define variable_h
+#ifndef variables_h
+#define variables_h
+
 //Main
-bool outputAngle = false;
-float setAnglePitch = 0,setAnglePitchLPF = 0;
-float setAngleRoll = 0,setAngleRollLPF = 0;
-uint32_t prevPrint = 0;
-uint8_t subTick = 0;
+static bool outputAngle = false;
+static float setAnglePitch = 0,setAnglePitchLPF = 0;
+static float setAngleRoll = 0,setAngleRollLPF = 0;
+
+static uint32_t prevPrint = 0;
+static uint8_t subTick = 0;
+
+// IMU
+static int16_t  gyroADC[3],accADC[3]; // RawGyroData,RawAccData,LPAccData
 
 // CONFIG VARIABLES
-int32_t pitchPID[3],rollPID[3]; 
-int8_t accDir[3] = {1,1,1};
-int8_t gyrDir[3] = {1,1,1};
-uint8_t pitchMotorPower = 0;
+static int32_t pitchPID[3],rollPID[3]; 
+static int8_t accDir[3] = {1,1,1};
+static int8_t gyrDir[3] = {1,1,1};
+static uint8_t pitchMotorPower = 0;
 
 //RC Decode
-volatile byte stateRCInt = 0;    // a counter to see how many times the pin has changed
-volatile uint32_t lastTimeRising = 0;
-volatile uint16_t lastPulse = 0;
-int16_t rcPulse1 = 0;
+volatile static byte stateRCInt = 0;    // a counter to see how many times the pin has changed
+volatile static uint32_t lastTimeRising = 0;
+volatile static uint16_t lastPulse = 0;
+static int16_t rcPulse1 = 0;
 #endif
