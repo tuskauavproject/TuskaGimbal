69ba1536cb1d34ba0d40776eea05550cdd57f183
arlarson-tuska <arlarson@crimson.ua.edu>
2015-09-16 00:07:19 -0700

    tweaked constants for better performance

--- Arduino/sketches/TGimbal/defines.h
+++ Arduino/sketches/TGimbal/defines.h
@@ -7,8 +7,8 @@
 #define SERIAL_BAUD_RATE 115200
 #define PWM_32KHZ_PHASE //PWM Frequency for motor control
 #define MOTORUPDATE_FREQ 1000 // in Hz, 1000 is default
-#define ACC_LPF_FACTOR 3   //magnitude of Acc data lowpass, filtering increases Acc lag
-#define GYROWEIGHT 0.990      // weight of gyro in complementary filter, out of 1 
+#define ACC_LPF_FACTOR 1   //magnitude of Acc data lowpass, filtering increases Acc lag
+#define GYROWEIGHT 0.997      // weight of gyro in complementary filter, out of 1 
 #define PS_LPF_FACTOR 1
 #define SUBTICK_FREQ 8
 #define RCPIN1 4
@@ -21,7 +21,8 @@
 #define PI_IN_THOUSANDTHS 3142 
 #define RADTODEG 57.2957795  
 #define RADTODEG_IN_THOUSANDTHS 57296 
-#define GYROSCALE ((2380 * PI)/((32767.0f / 4.0f ) * 180.0f * 1000000.0f)) //converts raw data units to radians, determined experimentally
+#define GYROSCALE ((2380 * PI)/((32767.0f / 4.0f ) * 180.0f * 1000000.0f))/4.0 //converts raw data units to radians, determined experimentally
+#define GYROSCALE2 3.0/(56.6 * 1000000)
 #define dt 1000 // dt in micros seconds
 #define LOOPUPDATE_FREQ MOTORUPDATE_FREQ     // loop control sample rate equals motor update rate
 #define DT_INT_MS (1000/MOTORUPDATE_FREQ)    // dT, integer, (ms)
